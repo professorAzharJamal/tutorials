b8e419208fa0f7f8f73cc1622c16a1e74044a051
professorAzharJamal <professorazharjamal@gmail.com>
2024-03-03 08:30:12 +0530

    commit

---
+++ react Excel/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import axios from 'axios'
+import './App.css';
+
+import {ExportToExcel} from './ExportToExcel'
+
+function App() {
+  const [data, setData] = React.useState([])
+  const fileName = "myfile"; // here enter filename for your excel file
+
+  React.useEffect(() => {
+    const fetchData = () =>{
+     axios.get('https://jsonplaceholder.typicode.com/posts').then(r => setData(r.data) )
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <div className="App">
+      <ExportToExcel apiData={data} fileName={fileName} />
+    </div>
+  );
+}
+
+export default App;
